98051a93f16db2c02381b79969acfeec64963960
Joan Puigcerver <joapuiib@gmail.com>
2021-10-26 19:52:13 +0200

    BASHRC GIT: removed GIT_CONFIG and used include.path

--- bashrc/install.sh
+++ bashrc/install.sh
@@ -4,3 +4,6 @@ USR_BASHRC=`pwd`/bashrc
 if ! grep -q "^source .*usr/bashrc/bashrc$" ~/.bashrc ; then
     echo "source $USR_BASHRC" | tee -a ~/.bashrc
 fi
+
+USR_GITCONFIG=`pwd`/gitconfig
+git config --global include.path $USR_GITCONFIG
