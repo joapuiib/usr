0fc9f167993e5dc6fed4c95f99965aa07423fbc0
Joan Puigcerver <joapuiib@gmail.com>
2021-12-08 19:13:41 +0100

    Updated spotify key

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,8 +8,9 @@
 # apt install alltray
 # apt install chromium-browser chromium-codecs-ffmpeg-extra
 
-# Key valid as 13/06/2021
-# curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+## https://www.spotify.com/es/download/linux/
+## Key valid as 08/12/2021
+# curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 # echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 # sudo apt-get update && sudo apt-get install spotify-client
 
